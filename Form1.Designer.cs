dc81a352be621043433992700ff9c525d0762c79
MastroPhaedra <64019897+MastroPhaedra@users.noreply.github.com>
2021-02-22 10:51:26 +0200

    Add files via upload
    + update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,11 @@
             this.HindBox = new System.Windows.Forms.TextBox();
             this.PiltBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -139,19 +143,53 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.button1.Location = new System.Drawing.Point(10, 381);
+            this.button1.Location = new System.Drawing.Point(12, 264);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(247, 57);
+            this.button1.Size = new System.Drawing.Size(247, 54);
             this.button1.TabIndex = 10;
-            this.button1.Text = "Primenit\'";
+            this.button1.Text = "Dobavit\'";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button2.Location = new System.Drawing.Point(12, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(247, 54);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Izmenit\'";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button3.Location = new System.Drawing.Point(12, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(247, 54);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(802, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(421, 370);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1235, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.PiltBox);
             this.Controls.Add(this.HindBox);
@@ -166,6 +204,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,6 +223,9 @@
         private System.Windows.Forms.TextBox HindBox;
         private System.Windows.Forms.TextBox PiltBox;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -34,6 +34,7 @@ namespace Forms_c_sharp
             adapter.Fill(table);
             dataGridView1.DataSource = table;
             con.Close();
+            pictureBox1.Image = Image.FromFile("../../Images/meme1.png");
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -51,25 +52,109 @@ namespace Forms_c_sharp
             if (dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString()=="")
             {
                 PiltBox.Text = "None";
+                pictureBox1.Image = Image.FromFile("../../Images/meme1.png");
             }
             else
             {
                 PiltBox.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                pictureBox1.Image = Image.FromFile("../../Images/"+dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString());
             }
         }
 
+        private void ClearData()
+        {
+            NimetusBox.Clear();
+            KogusBox.Clear();
+            HindBox.Clear();
+            PiltBox.Clear();
+            pictureBox1.Image = Image.FromFile("../../Images/meme1.png");
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            con.Open();
-            command = new SqlCommand("INSERT INTO Tooded(Nimetus, Kogus, Hind, Pilt) VALUES(@toode,@kogus,@hind,@pilt)",con);
-            command.Parameters.AddWithValue("@toode",NimetusBox.Text);
-            command.Parameters.AddWithValue("@kogus", KogusBox.Text);
-            command.Parameters.AddWithValue("@hind", HindBox.Text);
-            command.Parameters.AddWithValue("@pilt", PiltBox.Text);
-            command.ExecuteNonQuery();
-            con.Close();
-            Data();
+            if (NimetusBox.Text != "" && KogusBox.Text != "" && HindBox.Text != "")
+            {
+                try
+                {
+                    con.Open();
+                    command = new SqlCommand("INSERT INTO Tooded(Nimetus, Kogus, Hind, Pilt) VALUES(@toode,@kogus,@hind,@pilt)", con);
+                    command.Parameters.AddWithValue("@toode", NimetusBox.Text);
+                    command.Parameters.AddWithValue("@kogus", KogusBox.Text);
+                    command.Parameters.AddWithValue("@hind", HindBox.Text.Replace(",", "."));
+                    command.Parameters.AddWithValue("@pilt", PiltBox.Text.Replace(",", "."));
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    Data();
+                    ClearData();
+                    MessageBox.Show("Dannie dobavleni");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Oshibka s bazoi dannih!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Neobhodimo vvesti dannie!");
+            }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (NimetusBox.Text != "" && KogusBox.Text != "" && HindBox.Text != "")
+            {
+                try
+                {
+                    con.Open();
+                    command = new SqlCommand("UPDATE Tooded SET Nimetus=@toode,Kogus=@kogus,Hind=@hind,Pilt=@pilt WHERE Id=@id", con);
+                    command.Parameters.AddWithValue("@id", Id);
+                    command.Parameters.AddWithValue("@toode", NimetusBox.Text);
+                    command.Parameters.AddWithValue("@kogus", KogusBox.Text);
+                    command.Parameters.AddWithValue("@hind", HindBox.Text.Replace(",", "."));
+                    //command.Parameters.AddWithValue("@pilt", PiltBox.Text.Replace(",","."));
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    Data();
+                    ClearData();
+                    MessageBox.Show("Dannie obnovleni");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Oshibka s bazoi dannih!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Neobhodimo vibrat' dannie!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Id!=0)
+            {
+                try
+                {
+                    string andmed = "Andmed" + NimetusBox.Text + " on kustutatud";
+                    command = new SqlCommand("DELETE Tooded WHERE Id=@id", con);
+                    con.Open();
+                    command.Parameters.AddWithValue("@id", Id);
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    Data();
+                    ClearData();
+                    MessageBox.Show(andmed);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Oshibka s bazoi dannih!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Oshibka udalenia!");
+            }
+        }
+
 
 
         //private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
